1af4e3616170e4c293132250b8b44cb8b7732905
oronymzy <oronymzy@mostserio.us>
2019-06-09 01:13:39 -0400

    Add capability of displaying colorized text
    Rename 'random_selection' to 'random_6ws_index'.

--- figmentary.py
+++ figmentary.py
@@ -2,13 +2,23 @@ import argparse
 import random
 import ruamel.yaml
 
+colorful_available = True
+try: import colorful
+except ImportError: colorful_available = False
+
 yaml = ruamel.yaml.YAML()
 
 # Using the argparse module
 parser = argparse.ArgumentParser(prefix_chars='-+')
+parser.add_argument("-c", "--colorize", help="display colorized text (requires *colorful* package)", action="store_true")
 parser.add_argument("-t", "--tag", dest="minus_t", help="exclude literature by a specified tag", default=None)
 parser.add_argument("+t", "++tag", dest="plus_t", help="only include literature containing a specified tag", default=None)
 args = parser.parse_args()
+
+if args.colorize == True:
+    colorize_text = True
+else:
+    colorize_text = False
 
 if args.minus_t != None:
     tag_to_exclude = args.minus_t
@@ -37,5 +47,11 @@ with open("figmentary.yaml", "r") as opened_file:
         # Removing any list items in the 'six-word stories' list that do not contain the required tag
         changing_six_word_stories[:] = [list_item for list_item in changing_six_word_stories if required_tag in list_item['tag']]
     number_of_six_word_stories = len(contents_of_opened_file['six-word stories'])
-    random_selection = random.randint(0,number_of_six_word_stories - 1)
-    print(contents_of_opened_file['six-word stories'][random_selection]['story'])
+    random_6ws_index = random.randint(0,number_of_six_word_stories - 1)
+    if colorize_text == True and colorful_available == True:
+        colorful.use_style('solarized')
+        available_colors = ['yellow','orange','red','magenta','violet','blue','cyan','green']
+        random_color_selection = random.choice(available_colors)
+        print(getattr(colorful, random_color_selection),contents_of_opened_file['six-word stories'][random_6ws_index]['story'])
+    else:
+        print(contents_of_opened_file['six-word stories'][random_6ws_index]['story'])
