cdc0e067b463380234e081f50a0420ba3250423b
oronymzy <oronymzy@mostserio.us>
2019-06-09 05:38:27 -0400

    Add ability to display diagnostic information and to count stories
    Import 'sys' module.
    Add '--count-stories' and '--diagnostic' arguments.
    Change 'literature' to 'story'.
    Add comment mentioning filtering six-word stories based on user input.

--- figmentary.py
+++ figmentary.py
@@ -1,6 +1,7 @@
 import argparse
 import random
 import ruamel.yaml
+import sys
 
 colorful_available = True
 try: import colorful
@@ -10,10 +11,15 @@ yaml = ruamel.yaml.YAML()
 
 # Using the argparse module
 parser = argparse.ArgumentParser(prefix_chars='-+')
-parser.add_argument("-c", "--colorize", help="display colorized text (requires *colorful* package)", action="store_true")
-parser.add_argument("-t", "--tag", dest="minus_t", help="exclude literature by a specified tag", default=None)
-parser.add_argument("+t", "++tag", dest="plus_t", help="only include literature containing a specified tag", default=None)
+parser.add_argument("-c", "--colorize", help="display colorized text with a pseudorandomly-selected color (requires *colorful* package)", action="store_true")
+parser.add_argument("-C", "--count-stories", help="display a count of how many stories are available instead of displaying a story", action="store_true")
+parser.add_argument("-d", "--diagnostic", help="display diagnostic information instead of displaying a story", action="store_true")
+parser.add_argument("-t", "--tag", dest="minus_t", help="exclude story by a specified tag", default=None)
+parser.add_argument("+t", "++tag", dest="plus_t", help="only include story containing a specified tag", default=None)
 args = parser.parse_args()
+
+display_story = True
+display_diagnostic_information = True
 
 if args.colorize == True:
     colorize_text = True
@@ -30,8 +36,19 @@ if args.plus_t != None:
 else:
     required_tag = None
 
+if args.count_stories == True:
+    count_stories = True
+else:
+    count_stories = False
+
+if args.diagnostic == True:
+    display_diagnostic_information = True
+else:
+    display_diagnostic_information = False
+
 with open("figmentary.yaml", "r") as opened_file:
     contents_of_opened_file = yaml.load(opened_file)
+    # Filtering six-word stories based on user input
     if tag_to_exclude != None:
         # Two separate list comprehensions are required in order to avoid checking for a tag in a non-existent 'tag' key
         changing_six_word_stories = contents_of_opened_file['six-word stories']
@@ -47,11 +64,23 @@ with open("figmentary.yaml", "r") as opened_file:
         # Removing any list items in the 'six-word stories' list that do not contain the required tag
         changing_six_word_stories[:] = [list_item for list_item in changing_six_word_stories if required_tag in list_item['tag']]
     number_of_six_word_stories = len(contents_of_opened_file['six-word stories'])
-    random_6ws_index = random.randint(0,number_of_six_word_stories - 1)
-    if colorize_text == True and colorful_available == True:
-        colorful.use_style('solarized')
-        available_colors = ['yellow','orange','red','magenta','violet','blue','cyan','green']
-        random_color_selection = random.choice(available_colors)
-        print(getattr(colorful, random_color_selection),contents_of_opened_file['six-word stories'][random_6ws_index]['story'])
-    else:
-        print(contents_of_opened_file['six-word stories'][random_6ws_index]['story'])
+    if number_of_six_word_stories == 0:
+        # In this situation, no six-word stories remain
+        exit()
+    if display_diagnostic_information == True:
+        yaml.dump(contents_of_opened_file, sys.stdout)
+        count_stories = False
+        display_story = False
+    if count_stories == True:
+        print("Story count:",number_of_six_word_stories)
+        # Do not display a story now that story-count-related information has been displayed
+        display_story = False
+    if display_story == True:
+        random_6ws_index = random.randint(0,number_of_six_word_stories - 1)
+        if colorize_text == True and colorful_available == True:
+            colorful.use_style('solarized')
+            available_colors = ['yellow','orange','red','magenta','violet','blue','cyan','green']
+            random_color_selection = random.choice(available_colors)
+            print(getattr(colorful, random_color_selection),contents_of_opened_file['six-word stories'][random_6ws_index]['story'])
+        else:
+            print(contents_of_opened_file['six-word stories'][random_6ws_index]['story'])
